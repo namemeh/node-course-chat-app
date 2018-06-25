aade77438f51e171a7d99d98e572b1d008901466
Namik Mehuric <namik.mehuric@walter.ba>
2018-06-25 15:19:44 +0200

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const publicPath = path.join(__dirname,'../public');
 console.log("start app server.js");
 // console.log(__dirname + '/../public');
 // console.log(publicPath);
-const port = process.env.PORT || 3025;
+const port = process.env.PORT || 3030;
 var app = express();
 var server = http.createServer(app);
 
@@ -20,6 +20,17 @@ app.use(express.static(publicPath));
 
 io.on('connection',(socket)=>{
     console.log('New user connected');
+    socket.emit('newMessage', {
+        from: "Administrator",
+        text: "Welcome to the Chat app!",
+        createdAt: new Date().getTime(),
+    });
+    socket.broadcast.emit('newMessage', {
+        from: "Administrator",
+        text: "New user joined",
+        created: new Date().getTime()
+    });
+
 /*
     socket.emit('newEmail',{
         from: 'mike@example.com',
@@ -32,7 +43,7 @@ io.on('connection',(socket)=>{
         console.log('createEmail is ', newEmailData);
     });
 
-    */
+ */
 // emmitts event to single connection
 /*socket.emit('newMessage',{
     from: 'Jhon',
@@ -41,21 +52,22 @@ io.on('connection',(socket)=>{
 });
 */
 
-
 socket.on('createMessage', (message) => {
     console.log('createMessage is ', message);
-    // emitts to every single connection
-    io.emit('newMessage', {
+    // io.emit emitts to every single connection
+     io.emit('newMessage', {
         from: message.from,
         text: message.text,
-        createdt: new Date().getTime()
-    });
+       createdt: new Date().getTime()
+     });
+
+// send to everybody excpet one that created message
+//    socket.broadcast.emit('newMessage', {
+//            from: message.from,
+//           text: message.text,
+//           created: new Date().getTime()
+//    });
 });
-
-
-
-
-
 
 socket.on('disconnect',() =>{
     console.log('User disconnected from server!');
